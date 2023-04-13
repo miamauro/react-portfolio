e24fc738eef6670bac0b0b56ef121bbaa980b762
miamauro <miiamauro@outlook.com>
2023-04-13 00:59:12 -0500

    added headshot to about component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,17 @@
 import React from "react";
+import headshot from "../../images/headshot.jpeg";
 
 function About() {
   return (
-    <p>
-      Hello! My name is Mia Mauro, and I'm currently studying web development in
-      the University of Denver's Full-Stack Coding program. I live in Des
-      Moines, Iowa, but was previously traveling the country full time as a
-      registered nurse.
-    </p>
+    <div>
+      <img src={headshot} alt="headshot" height="300px"></img>
+      <h1>
+        Hello! My name is Mia Mauro, and I'm currently studying web development
+        in the University of Denver's Full-Stack Coding program. I live in Des
+        Moines, Iowa, but was previously traveling the country full time as a
+        registered nurse.
+      </h1>
+    </div>
   );
 }
 
