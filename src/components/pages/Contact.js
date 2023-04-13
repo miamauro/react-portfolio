8402cc77e2aebdba34308d287b3f63859faf2677
miamauro <miiamauro@outlook.com>
2023-04-13 01:27:09 -0500

    inserted form into contact component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <form>
+      <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input type="name" class="form-control" aria-describedby="name" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Email address</label>
+        <input type="email" class="form-control" aria-describedby="email" />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Message</label>
+        <textarea class="form-control" />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Contact Me
+      </button>
+    </form>
+  );
 }
 
 export default Contact;
