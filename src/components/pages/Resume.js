9b73c8f7447d851657c8f445ee268f903decc46e
miamauro <miiamauro@outlook.com>
2023-04-13 23:28:04 -0500

    added icon to resume component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,30 +5,43 @@ import "./Resume.css";
 function Resume() {
   return (
     <>
-      <a href={pdf} target="_blank">
-        Download my resume.
+      <a href={pdf} target="_blank" id="res">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="26"
+          height="26"
+          fill="currentColor"
+          className="bi bi-file-earmark-arrow-down"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293V6.5z" />
+          <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+        </svg>
+        My Resume
       </a>
-      <div>
-        <h2>Front-End Skills</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
-      </div>
-      <div>
-        <h2>Back-End Skills</h2>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL/Sequelize</li>
-          <li>MongoDB/Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
+      <div id="skills">
+        <div id="front">
+          <h2>Front-End Skills</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div id="back">
+          <h2>Back-End Skills</h2>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL/Sequelize</li>
+            <li>MongoDB/Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
       </div>
     </>
   );
