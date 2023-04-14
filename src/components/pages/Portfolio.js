b07b470a51c2b20038fd74db4108209fa9fa29cd
miamauro <miiamauro@outlook.com>
2023-04-13 22:50:03 -0500

    added projects to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,10 @@ import React from "react";
 import Project from "../Project/Project";
 import travelBuddy from "../../images/travbuddy.png";
 import discoveringDenver from "../../images/discdenv.png";
+import regexTut from "../../images/regextut.png";
+import socialNetApi from "../../images/socialnetapi.png";
+import pwGen from "../../images/pwgen.png";
+import codeQuiz from "../../images/codequiz.png";
 
 const projects = [
   {
@@ -17,28 +21,28 @@ const projects = [
     img: discoveringDenver,
   },
   {
-    title: "Travel Buddy",
-    link: "https://hollyhleal.github.io/travel-buddy/",
-    repo: "https://github.com/miamauro/travel-buddy",
-    img: travelBuddy,
+    title: "Regex Tutorial",
+    link: "https://gist.github.com/miamauro/b1f26102b757cc305c9faa6a9dd4f36f",
+    repo: "https://github.com/miamauro/regex-tutorial",
+    img: regexTut,
   },
   {
-    title: "Discovering Denver",
-    link: "https://whispering-dawn-72162.herokuapp.com/",
-    repo: "https://github.com/miamauro/discovering-denver",
-    img: discoveringDenver,
+    title: "Social Network API",
+    link: "https://drive.google.com/file/d/1uvdGuYut-pIYrWE2jm15e9577JuMg7u5/view",
+    repo: "https://github.com/miamauro/social-network-api",
+    img: socialNetApi,
   },
   {
-    title: "Travel Buddy",
-    link: "https://hollyhleal.github.io/travel-buddy/",
-    repo: "https://github.com/miamauro/travel-buddy",
-    img: travelBuddy,
+    title: "Password Generator",
+    link: "https://miamauro.github.io/password-generator/",
+    repo: "https://github.com/miamauro/password-generator",
+    img: pwGen,
   },
   {
-    title: "Discovering Denver",
-    link: "https://whispering-dawn-72162.herokuapp.com/",
-    repo: "https://github.com/miamauro/discovering-denver",
-    img: discoveringDenver,
+    title: "Code Quiz",
+    link: "https://miamauro.github.io/code-quiz/",
+    repo: "https://github.com/miamauro/code-quiz",
+    img: codeQuiz,
   },
 ];
 
