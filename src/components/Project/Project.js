5f517bb334d016a2da0a827961bb2a1e61e70aad
miamauro <miiamauro@outlook.com>
2023-04-17 20:13:22 -0500

    turned app links on each card into buttons in project component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,16 +5,16 @@ function Project(props) {
   return (
     <div className="card" style={{ width: 425, margin: 15 }}>
       <img src={props.img} class="card-img-top h-100" alt={props.title} />
-      <div className="card-body">
+      <div className="card-body app-title">
         <h2 className="card-title">{props.title}</h2>
         <p class="card-text">{props.text}</p>
       </div>
-      <div className="card-body">
-        <a href={props.link} target="_blank" className="card-link">
+      <div className="card-body app-links">
+        <a href={props.link} target="_blank" className="btn btn-primary" id="app-link">
           Application
         </a>
-        <a href={props.repo} target="_blank" className="card-link">
-          Repo
+        <a href={props.repo} target="_blank" className="btn btn-primary" id="app-link">
+          Repository
         </a>
       </div>
     </div>
