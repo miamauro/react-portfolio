909c5af1ca962ba6d3e819341f6a46df6f9dcc84
miamauro <miiamauro@outlook.com>
2023-04-26 14:39:21 -0500

    fixed grammar in about component

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -9,7 +9,7 @@ function About() {
       <h2 id="about">
         Hello! My name is Mia Mauro, and I'm currently studying web development
         in the University of Denver's Full-Stack Coding program. I live in Des
-        Moines, Iowa, but was previously traveling the country full time as a
+        Moines, Iowa, but was previously traveling the country full-time as a
         registered nurse. <br /> <br />
         After four years in the healthcare industry, my love for learning lead
         me back to the classroom. I am thrilled to utilize my critical thinking
