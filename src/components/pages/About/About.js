0215b6165cb6caff4385cbca25a6b5130c515fbb
miamauro <miiamauro@outlook.com>
2023-04-27 19:15:27 -0500

    updated About component to include program completion

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -7,10 +7,10 @@ function About() {
     <div>
       <img src={headshot} alt="headshot" id="headshot"></img>
       <h2 id="about">
-        Hello! My name is Mia Mauro, and I'm currently studying web development
-        in the University of Denver's Full-Stack Coding program. I live in Des
-        Moines, Iowa, but was previously traveling the country full-time as a
-        registered nurse. <br /> <br />
+        Hello! My name is Mia Mauro, and I've recently completed the University
+        of Denver's Full-Stack Web Development program. I live in Des Moines,
+        Iowa, but was previously traveling the country full-time as a registered
+        nurse. <br /> <br />
         After four years in the healthcare industry, my love for learning lead
         me back to the classroom. I am thrilled to utilize my critical thinking
         skills - along with a diverse range of talents - in a whole new light! I
